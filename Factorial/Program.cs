8f49f7ca8e93aedd991def26ba207faed059ab39
Nazarets Ksenia <ksenianazarec995@gmail.com>
2024-09-09 08:43:40 -0700

    extra int deleted

--- Factorial/Program.cs
+++ Factorial/Program.cs
@@ -14,19 +14,21 @@ namespace Factorial
     internal class Program
     {
  
-       
-      
+       static int factorial(int v)
+        {
+            for (int i = v-1; i > 0; --i) v *= i;
+            return v;
+        }
+
         static void Main(string[] args)
         {
             Console.Write("Enter your number: ");
             int v = Convert.ToInt32(Console.ReadLine());
-           // for (int i = v-1; i>0 ; --i)v *= i;
-            Console.WriteLine($" it's {v}.");
+            Console.WriteLine($" it's {factorial(v)}.");
 
             BigInteger factor = 1;
             for (int i = v; i > 0; --i)factor *= i;
             Console.WriteLine($" its factorial is {factor}.");
-
         }
     }
 }
