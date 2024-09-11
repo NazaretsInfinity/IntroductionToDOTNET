7fca4a3aba0e5f9c55e03d0f624c53e4c6758dcb
Nazarets Ksenia <ksenianazarec995@gmail.com>
2024-09-11 09:56:50 -0700

    edited simple calc

--- game/Program.cs
+++ game/Program.cs
@@ -13,53 +13,78 @@ namespace game
         {
 
 
-#if game
+#if true1
             Random rand = new Random();
             int x = (rand.Next(Console.WindowWidth));
             int y = (rand.Next(Console.WindowHeight));
+            Console.WriteLine($"X = {x}   Y = {y}");
+         
             Console.SetCursorPosition(x, y);
 
+
             ConsoleKey key;
+            Console.CursorVisible = false;
+            //CursorVisible - false = to make the cursor unseen on the console
+            char symbol = (char)2;
+            Console.WriteLine(symbol);
             do
             {
+                
                 key = Console.ReadKey(true).Key;
                 switch (key)
                 {
-                    case ConsoleKey.RightArrow: Console.SetCursorPosition(++x, y); break;
-                    case ConsoleKey.D: Console.SetCursorPosition(++x, y); break;
-                    case ConsoleKey.LeftArrow: Console.SetCursorPosition(x--, y); break;
-                    case ConsoleKey.A: Console.SetCursorPosition(x--, y); break;
-                    case ConsoleKey.UpArrow: Console.SetCursorPosition(x, --y); break;
-                    case ConsoleKey.W: Console.SetCursorPosition(x, --y); break;
-                    case ConsoleKey.DownArrow: Console.SetCursorPosition(x, ++y); break;
-                    case ConsoleKey.S: Console.SetCursorPosition(x, ++y); break;
+                    case ConsoleKey.RightArrow: ++x; break;
+                    case ConsoleKey.D: ++x; break;
+                    case ConsoleKey.LeftArrow: --x; break;
+                    case ConsoleKey.A: --x; break;
+                    case ConsoleKey.UpArrow: --y; break;
+                    case ConsoleKey.W: y--; break;
+                    case ConsoleKey.DownArrow: ++y; break;
+                    case ConsoleKey.S: ++y; break;
                 }
-            } while (key != ConsoleKey.Escape); 
+                if (y < 1) y = 0;
+                if (x < 0) x = 0;
+                if (y > Console.BufferHeight - 1) y = Console.BufferHeight - 1;
+                if (x > Console.BufferWidth - 1) y = Console.BufferWidth - 1;
+                Console.Clear();
+                Console.WriteLine($"X = {x}   Y = {y}");
+                Console.SetCursorPosition(x, y);
+                Console.Write(symbol);
+            } while (key != ConsoleKey.Escape);
+           
 #endif
 
+#if true
             Console.Write("Enter what to solve: ");
             string s = Console.ReadLine();
-            char[] dels = { '+', '-', '*', '/', '=' };
-            
-            string[] tokens = s.Split(dels);
 
-
+            string[] tokens = s.Split('+', '-', '*', '/', '=');
 
             double result = 0;
             double a = Convert.ToDouble(tokens[0]);
             double b = Convert.ToDouble(tokens[1]);
-           
+
+            if (s.Contains('+')) result = a + b;
+            //'Contains' determines if the string have a symbol or substring(type it in parameters) 
+            else if (s.Contains("-")) result = a - b;
+            else if (s.Contains("*")) result = a * b;
+            else if (s.Contains("/")) result = a / b;
+            else Console.Write("No ar.operations.  ");
+
+            Console.WriteLine($"It's {result}"); 
+#endif
 
 
-            if (s.Contains('+')) result = a+b;
-            else if (s.Contains("-")) result = a-b;
-            else if (s.Contains("*")) result = a*b;
-            else if (s.Contains("/")) result = a/b;
-
-            Console.WriteLine($"It's {result}");
 
 
-#if inprogress
+#if true3
+
+
+      Console.Write("Enter what to solve: ");
+            string s = Console.ReadLine();
+            char[] dels = { '+', '-', '*', '/', '=' };
+            
+            string[] tokens = s.Split(dels);
 
 
             double[] numbers = new double[tokens.Length];
@@ -89,6 +114,8 @@ namespace game
 
             }
               Console.WriteLine($"It's {result}");
+
+              numbers = null;
 #endif
         }
     }
